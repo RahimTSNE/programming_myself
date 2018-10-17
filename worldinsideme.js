8fe89a9c23082d2b72bae3330f287b77ef5bcfca
RahimTSNE <rahimtsne@gmail.com>
2018-10-17 09:25:40 +0530

    Update worldinsideme.js

--- worldinsideme.js
+++ worldinsideme.js
@@ -1,1 +1,4 @@
 
+function rightNow(thinking){
+  document.write("I am not yet successful and their is surely a drive to be the one but what if i will not be the one then my case will be the case study for how not to do things. to let everyone to study me I am starting to document my life. Places: github, youtube);
+}
