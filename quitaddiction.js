53b8ecf43389541df1e07dac4cef59e4936714a0
RahimTSNE <rahimtsne@gmail.com>
2018-10-17 09:48:55 +0530

    Create quitaddiction.js

---
+++ quitaddiction.js
@@ -0,0 +1,15 @@
+function quitAddiction(){
+//gutkha is betelnut mixed with tobacco
+
+    if(thinking(rightNow, gutkha){
+// compare the value of gutkha and vegetable
+    if ("gutkha" => ("vegetable" || "fruit")){
+        document.write("you can buy gutkha and as much as you can.");
+    }
+    if("gutkha" < ("vegetable" || "fruit")){
+        document.write("It is injurious to health. Avoid it at any cost.");
+    }
+
+
+
+}
